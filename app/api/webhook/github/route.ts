d9c76b22ed562ee1b18b18a249ef7016eea74dfe
Ayushbh686 <Ayushbh686@gmail.com>
2026-09-04 17:31:16 +0530

    10-codehorse reviews implementaion inngest

--- app/api/webhook/github/route.ts
+++ app/api/webhook/github/route.ts
@@ -1,3 +1,4 @@
+import { reviewPullRequest } from "@/module/ai/actions";
 import { error } from "console";
 import { NextResponse, NextRequest } from "next/server";
 
@@ -5,25 +6,35 @@ export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
     const event = req.headers.get("x-github-event");
-    console.log(`Received Github event : ${event}`)
+    console.log(`Received Github event : ${event}`);
 
     if (event === "ping") {
-      return NextResponse.json(
-        { message: "Pong" },
-        { status: 200 }
-      );
+      return NextResponse.json({ message: "Pong" }, { status: 200 });
     }
     // TODO: HANDLE LATER
 
-    return NextResponse.json(
-      { message: "Event Processes" },
-      { status: 200 }
-    );
+    if (event === "pull_request") {
+      const action = body.action;
+      const repo = body.repository.full_name;
+      const prNumber = body.number;
+
+      const [owner, repoName] = repo.split("/");
+
+      if (action === "opened" || action === "synchronize") {
+        reviewPullRequest(owner, repoName, prNumber)
+          .then(() => console.log(`Review completed for ${repo} #${prNumber}`))
+          .catch((error) =>
+            console.log(`Review Failed for ${repo} #${prNumber}`),
+          );
+      }
+    }
+
+    return NextResponse.json({ message: "Event Processes" }, { status: 200 });
   } catch (error) {
     console.error("Error processing webhook:", error);
     return NextResponse.json(
       { error: "Internal Server Error" },
-      { status: 500 }
+      { status: 500 },
     );
   }
 }
